6ccb7aacee0970914a659fd47c9b559bfca58d13
Wayne Oliver <wayne@instacom.co.za>
2024-06-26 09:45:14 +0200

    fix up tests

--- test/test-logging.js
+++ test/test-logging.js
@@ -1,28 +1,32 @@
-const fs = require("fs");
-var assert = require('assert');
+const fs = require("fs")
+var assert = require("assert")
 
-var log = require('../index.js')
-log.setLevel(5);
-log.setPath('./test')
-log.setPrefix('test_');
+var log = require("../index.js")
+log.setLevel(5)
+log.setPath("./test")
+log.setPrefix("test_")
 
-log.info("hello world!");
+log.info("hello world!")
 
-var fileName = log.getFileName();
+var fileName = log.getFileName()
 
-describe('The log file', function() {
-    it('should exists in the prescribed path', function() {
-        assert.equal(true,fs.existsSync(fileName));
-    }),
-  
-    it('contians the message logged above', function() {
-        var logstring = data = fs.readFileSync(fileName);
-        assert.equal(true,logstring.includes("hello world!"));
+describe("The log file", function () {
+    it("should exists in the prescribed path", function () {
+        assert.equal(true, fs.existsSync(fileName))
     })
-  
-    
-    it('has been deleted', function() {
-        fs.unlinkSync(fileName);
-        assert.equal(false,fs.existsSync(fileName));
+
+    it("contains the message logged above", function () {
+        var logString = (data = fs.readFileSync(fileName))
+        assert.equal(true, logString.includes("hello world!"))
     })
-});
+
+    it("uses the correct log level", function () {
+        var logString = (data = fs.readFileSync(fileName))
+        assert.equal(true, logString.includes("- INFO  -"))
+    })
+
+    it("has been deleted", function () {
+        fs.unlinkSync(fileName)
+        assert.equal(false, fs.existsSync(fileName))
+    })
+})
